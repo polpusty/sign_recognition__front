ae8f29a7678d608e75e80a4ecf1a38c7397b5add
Marcin Świerczyna <polpusty@gmail.com>
2019-01-09 18:10:52 +0100

    created application layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.png';
+import logo_short from './logo-short.png';
+import { Layout, Menu, Icon } from 'antd';
+
+const { Header, Sider, Content } = Layout;
 
 class App extends Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <Layout>
+          <Sider
+              trigger={null}
+              collapsible
+              collapsed={this.state.collapsed}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
+            <div className="logo"><img src={this.state.collapsed ? logo_short : logo} height="100%" width="100%" alt=""/></div>
+              <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1">
+                <Icon type="folder" />
+                <span>Collections</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="fork" />
+                <span>Neural networks</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Header style={{ background: '#fff', padding: 0 }}>
+              <Icon
+                  className="trigger"
+                  type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                  onClick={this.toggle}
+              />
+            </Header>
+            <Content style={{
+              margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280,
+            }}
+            >
+              Content
+            </Content>
+          </Layout>
+        </Layout>
     );
   }
 }
